bbe7bcacd33e2e0da1b3acddf3f62d144b50c3d8
dwcha <138362253+ttwok@users.noreply.github.com>
2024-07-30 11:53:20 +0900

    Update test_billbill.py

--- test_billbill.py
+++ test_billbill.py
@@ -5,25 +5,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
+import time
 
 def perform_automation(row, status_text):
-    USERNAME = 'bsuser_M3SxCe'
-    ACCESS_KEY = 'y8fswxT4kr1WSSPVCNeS'
-    URL = f"https://{USERNAME}:{ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub"
+    grid_url = "http://localhost:4444/wd/hub"
 
-    desired_cap = {
-        'os': 'Windows',
-        'os_version': '10',
-        'browser': 'Chrome',
-        'browser_version': 'latest',
-        'name': 'Streamlit Test',
-        'browserstack.debug': 'true'
-    }
-
-    driver = webdriver.Remote(
-        command_executor=URL,
-        desired_capabilities=desired_cap
-    )
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Remote(command_executor=grid_url, options=options)
 
     try:
         driver.get('https://siic-admin-local.cafe24.com/admin/sic/rvn/rvn_bil_tab.php')
@@ -34,7 +22,7 @@ def perform_automation(row, status_text):
         id_input = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@name="adm_id"]')))
         id_input.send_keys(row['아이디'])
 
-        password_input = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@name="adm_pw"]')))
+        password_input = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@name="비밀번호"]')))
         password_input.send_keys(row['비밀번호'])
 
         login_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@class="btnLogin"]/span')))
@@ -43,7 +31,7 @@ def perform_automation(row, status_text):
         status_text.text("로그인 시도 중...")
 
         # 로그인 성공 후 작업 진행
-        WebDriverWait(driver, 10).until(EC.url_contains('rvn_bil_tab.php'))
+        WebDriverWait(driver, 10).until(EC.url_changes('https://siic-admin-local.cafe24.com/admin/sic/rvn/rvn_bil_tab.php'))
         status_text.text("로그인 성공")
 
         # 이후 자동화 작업 코드 작성
@@ -58,28 +46,16 @@ def perform_automation(row, status_text):
 
 st.title('정산서 업로드 자동화')
 
-# 로그인 정보 입력 받기
-st.sidebar.header('로그인 정보')
-user_id = st.sidebar.text_input('아이디', 'dwcha')
-password = st.sidebar.text_input('비밀번호', 'cafe24@002', type='password')
-
-# 파일 업로드 위젯
 uploaded_file = st.file_uploader("엑셀 파일을 업로드하세요", type=["xlsx"])
 
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
-
-    # 아이디와 비밀번호를 데이터프레임에 추가
-    df['아이디'] = user_id
-    df['비밀번호'] = password
-
     st.write("업로드된 데이터:")
     st.dataframe(df)
 
-    if st.button('자동화 시작'):
-        for index, row in df.iterrows():
-            status_text = st.empty()
-            status_text.text(f"{row['쇼핑몰명']} 자동화 진행 중...")
-            perform_automation(row, status_text)
+    for index, row in df.iterrows():
+        status_text = st.empty()
+        status_text.text(f"{row['쇼핑몰명']} 자동화 진행 중...")
+        perform_automation(row, status_text)
 else:
     st.warning("엑셀 파일을 업로드해주세요.")
