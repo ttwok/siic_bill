b03832a0fc87749ffb85700a95ffa27e6ce785e2
dwcha <138362253+ttwok@users.noreply.github.com>
2024-07-30 14:25:46 +0900

    Update test_billbill.py

--- test_billbill.py
+++ test_billbill.py
@@ -18,6 +18,6 @@ options.add_argument("--disable-gpu")
 options.add_argument("--headless")
 
 driver = get_driver()
-driver.get("http://example.com")
+driver.get("https://siic-admin-local.cafe24.com/admin/sic/usr/usr_adm_loi_0.php?tab_no=0")
 
 st.code(driver.page_source)
