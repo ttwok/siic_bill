4406ca1a5e9bf78b5969b9f03ff4d1f770d12ae9
dwcha <138362253+ttwok@users.noreply.github.com>
2024-07-30 18:26:35 +0900

    Update test_billbill.py

--- test_billbill.py
+++ test_billbill.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
-st.title("Embed a Website in Streamlit")
+# wide 화면 레이아웃 설정
+st.set_page_config(layout="wide")
+
+st.title("Embed a Website in Streamlit - Wide Screen")
 
 # 특정 사이트 URL
 url = "https://siic-admin-local.cafe24.com/admin/sic/mbr/mbr_wdr_tab.php"
 
-# 아이프레임 생성
-st.components.v1.iframe(src=url, width=800, height=600)
+# wide 화면에 맞춰 아이프레임 생성
+st.components.v1.iframe(src=url, width=1400, height=800)
