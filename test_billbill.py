21bf1ba6e283ba24411fc2023eb4369f538c72de
dwcha <138362253+ttwok@users.noreply.github.com>
2024-07-30 13:15:28 +0900

    Update test_billbill.py

--- test_billbill.py
+++ test_billbill.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import webbrowser
 
 st.title("Automated Login to Cafe24")
 
@@ -8,8 +7,37 @@ user_id = st.text_input("Enter your user ID", "dwcha")
 password = st.text_input("Enter your password", "cafe24@002", type="password")
 login_url = "https://siic-admin-local.cafe24.com/admin/sic/mbr/mbr_wdr_tab.php"
 
-# 버튼 클릭 시 URL 열기
-if st.button("Open Cafe24 Login Page"):
-    # 사용자가 직접 로그인하도록 안내
-    webbrowser.open_new_tab(login_url)
-    st.info("Opened Cafe24 login page. Please enter your login details manually.")
+# 자바스크립트 코드 작성: 로그인 페이지 열고, 폼에 입력 값 설정
+js_code = f"""
+    <script>
+        function openAndLogin() {{
+            var url = "{login_url}";
+            var newWindow = window.open(url, '_blank');
+            newWindow.onload = function() {{
+                setTimeout(function() {{
+                    var userIdInput = newWindow.document.querySelector('input[name="adm_id"]');
+                    var passwordInput = newWindow.document.querySelector('input[name="adm_pw"]');
+                    if (userIdInput && passwordInput) {{
+                        userIdInput.value = "{user_id}";
+                        passwordInput.value = "{password}";
+                        var loginButton = newWindow.document.querySelector('button.btnLogin span');
+                        if (loginButton) {{
+                            loginButton.click();
+                        }} else {{
+                            var form = newWindow.document.querySelector('form');
+                            if (form) {{
+                                form.submit();
+                            }}
+                        }}
+                    }}
+                }}, 1000);
+            }};
+        }}
+        openAndLogin();
+    </script>
+"""
+
+# 버튼 클릭 시 자바스크립트 실행
+if st.button("Login to Cafe24"):
+    st.components.v1.html(js_code, height=0)
+    st.success("Login process initiated. Please check the new tab.")
