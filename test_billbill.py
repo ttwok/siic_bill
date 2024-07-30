272f13e5519c706dcd2d790c8d133652d3c14f1a
dwcha <138362253+ttwok@users.noreply.github.com>
2024-07-30 14:29:04 +0900

    Update test_billbill.py

--- test_billbill.py
+++ test_billbill.py
@@ -1,21 +1,21 @@
+import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
-@st.cache_resource
+@st.experimental_singleton
 def get_driver():
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
     return webdriver.Chrome(
         service=Service(
             ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
         ),
         options=options,
     )
-
-options = Options()
-options.add_argument("--disable-gpu")
-options.add_argument("--headless")
 
 driver = get_driver()
 driver.get("https://siic-admin-local.cafe24.com/admin/sic/usr/usr_adm_loi_0.php?tab_no=0")
