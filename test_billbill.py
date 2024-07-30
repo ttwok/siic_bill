4c5aae1d98994ef041004fd145e96a7392e2e996
dwcha <138362253+ttwok@users.noreply.github.com>
2024-07-30 14:31:53 +0900

    Update test_billbill.py

--- test_billbill.py
+++ test_billbill.py
@@ -1,25 +1,17 @@
+import streamlit as st
+import os, sys
+
+@st.experimental_singleton
+def installff():
+  os.system('sbase install geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+_ = installff()
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ECfrom             
-selenium.webdriver.chrome.options import Options
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-gpu")
+from selenium.webdriver import FirefoxOptions
+opts = FirefoxOptions()
+opts.add_argument("--headless")
+browser = webdriver.Firefox(options=opts)
 
-
-#Open new browser window
-driver = webdriver.Chrome(options=chrome_options)
-
-#Browser goes to auth_url
-driver.get(auth_url)
-
-#Sets up waiting until the second url to copy the new url
-wait = WebDriverWait(driver, 170)
-wait.until(EC.url_contains("code="))
-url = driver.current_url
-
-#closes window
-driver.close()
+browser.get('http://example.com')
+st.write(browser.page_source)
