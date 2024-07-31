d983b80aaba71abaa7fc711df33870f242bb6e81
dwcha <138362253+ttwok@users.noreply.github.com>
2024-07-31 13:18:44 +0900

    Update test_billbill.py

--- test_billbill.py
+++ test_billbill.py
@@ -1,24 +1,17 @@
 import streamlit as st
-from playwright.sync_api import sync_playwright
+import os, sys
 
-st.set_page_config(layout="wide")
-st.title("Embed a Website in Streamlit with Playwright")
+@st.experimental_singleton
+def installff():
+  os.system('sbase install geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
-def run(playwright):
-    browser = playwright.chromium.launch(headless=True, args=['--no-sandbox', '--disable-setuid-sandbox'])
-    page = browser.new_page()
-    page.goto('https://siic-admin-local.cafe24.com/admin/sic/rvn/rvn_bil_tab.php')
-    page.fill('input[name="username"]', 'your_username')  # 실제 아이디로 변경
-    page.fill('input[name="password"]', 'your_password')  # 실제 비밀번호로 변경
-    page.click('button[type="submit"]')
-    page.wait_for_load_state('networkidle')  # 페이지 로드 완료 대기
-    content = page.content()
-    browser.close()
-    return content
+_ = installff()
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
+opts = FirefoxOptions()
+opts.add_argument("--headless")
+browser = webdriver.Firefox(options=opts)
 
-try:
-    with sync_playwright() as playwright:
-        html_content = run(playwright)
-        st.components.v1.html(html_content, width=1400, height=800)
-except Exception as e:
-    st.error(f"An error occurred: {e}")
+browser.get('http://example.com')
+st.write(browser.page_source)
