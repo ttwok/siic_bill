df50d33780d0d2ba39a6945ae82e17f34bb3b79c
dwcha <138362253+ttwok@users.noreply.github.com>
2024-07-31 09:58:26 +0900

    Update test_billbill.py

--- test_billbill.py
+++ test_billbill.py
@@ -1,10 +1,13 @@
 import streamlit as st
+import requests
 
 st.set_page_config(layout="wide")
 st.title("Embed a Website in Streamlit")
 
-# 특정 사이트 URL
 url = "https://siic-admin-local.cafe24.com/admin/sic/mbr/mbr_wdr_tab.php"
 
-# 아이프레임 생성
-st.components.v1.iframe(src=url, width=1400, height=800)
+response = requests.get(url)
+if response.status_code == 200:
+    st.components.v1.html(response.text, width=1400, height=800)
+else:
+    st.error("Failed to load the page")
