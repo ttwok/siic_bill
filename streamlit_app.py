bdeab2ff1a862487073ab537596c05068bf8ef8a
dwcha <138362253+ttwok@users.noreply.github.com>
2024-07-31 13:28:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,6 @@ with st.echo():
     options.add_argument("--headless")
 
     driver = get_driver()
-    driver.get("http://example.com")
+    driver.get("https://siic-admin-local.cafe24.com/admin/sic/usr/usr_adm_loi_0.php?tab_no=0")
 
     st.code(driver.page_source)
